96193721655ac97270608e0411a061b00392e06b
Kjangir1910 <129332297+Kjangir1910@users.noreply.github.com>
2024-09-23 02:29:55 +0530

    Create Register.js

---
+++ src/Register.js
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import './App.css';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
+const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('')
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            const response = await axios.post('https://activant-2-backend-1.onrender.com/api/users/api/users/register', {
+                name, 
+                email,
+                address, 
+                password
+            })
+            setMessage('Registration successful')
+            navigate('/products');
+        } catch (error) {
+            setMessage('Error registering user')
+            console.error('Registration error', error)
+        }
+        setLoading(false);
+    }
+    
+
+    return (
+        <>
+        <div className="auth-container">
+            <form onSubmit={handleSubmit}>
+                <h2>Register User</h2>
+                <input type="text" 
+                placeholder="Name" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)}
+                required
+               />
+
+                 <input type="email" 
+                placeholder="Email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}
+                required
+               />
+
+                 <input type="text" 
+                placeholder="Address" 
+                value={address} 
+                onChange={(e) => setAddress(e.target.value)}
+                required
+               />
+
+                 <input type="password" 
+                placeholder="Password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}
+                required
+               />
+
+               <button type="submit" disabled={loading}>{loading ? <div className="loader"></div> : 'Register'}</button>
+               {message && <p>{message}</p>}
+            </form>
+        </div>
+        </>
+    )
+}
+
+export default Register
