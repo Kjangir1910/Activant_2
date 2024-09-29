40b2989198469d3b6f0bb84df8d2e058423b21ab
Kjangir1910 <129332297+Kjangir1910@users.noreply.github.com>
2024-09-30 02:33:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import Products from './Products';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
+
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Login />} />
+//         <Route path='/register' element={<Register />} />
+//         <Route path='/products' element={<Products />} />
+   
+
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/products' element={<Products />} />
-   
-
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
-}
+};
 
 export default App;
